5663c5ff4b4f54ae5fc7a0de22d95600c1e7ffd0
Borys <110242454+BorysMax@users.noreply.github.com>
2023-02-20 17:37:11 +0200

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,26 +1,26 @@
 class GuessingGame {
-  constructor() {
-    this.min = 0;
-    this.max = 0;
-  }
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+    }
 
-  setRange(min, max) {
-    this.min = min;
-    this.max = max;
-  }
+    setRange(min, max) {
+        this.min = min;
+        this.max = max;
+    }
 
-  guess() {
-    this.guessing = Math.round((this.max + this.min) / 2);
-    return this.guessing;
-  }
+    guess() {
+      this.guessing = Math.round((this.min + this.max) / 2);
+      return this.guessing;
+    }
 
-  lower() {
-    this.max = this.guessing;
-  }
+    lower() {
+        this.max = this.guessing;
+    }
 
-  greater() {
-    this.min = this.guessing;
-  }
+    greater() {
+        this.min = this.guessing;
+    }
 }
 
 module.exports = GuessingGame;
